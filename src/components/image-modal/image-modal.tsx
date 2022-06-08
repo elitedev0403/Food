e6492996c2191527e482d476302da0c782865bc8
elitedev0403 <iavosedra1108@gmail.com>
2022-06-08 21:18:45 +0200

    fixed minor issues

--- src/components/image-modal/image-modal.tsx
+++ src/components/image-modal/image-modal.tsx
@@ -17,7 +17,7 @@ const Modal:React.FC<ModalProps> = ({ show, imageUrl, onClose }) => {
 		show ?
 		<div className="fixed flex bg-overlay justify-center items-center top-0 left-0 w-full h-full py-10">
 			<a className="absolute hover:text-gray-300 text-white top-5 right-10 font-mono font-extrabold text-4xl" href="" onClick={handleCloseClick}>&times;</a>
-			<img src={imageUrl} alt="" className="sm:w-1/2 max-w-lg h-[calc(90vh-80px)]" />
+			<img src={imageUrl} alt="" className="sm:w-1/2 sm:max-w-lg max-w-[calc(100vw-80px)] h-[calc(90vh-80px)]" />
 		</div> : null
   );
 };
